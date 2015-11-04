83009d881c3193242bdbac6648de7771f29619de
Quan Hoang Van (framgia) <hoang.van.quan@framgia.com>
2015-11-04 13:25:37 +0700

    demo reactjs with rails

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+# app/controllers/comments_controller.rb
+class CommentsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Comment.all
+  end
+
+  def create
+    respond_with Comment.create(comment_params)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author, :comment)
+  end
+end
